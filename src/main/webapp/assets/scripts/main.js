c608c5a8d4c03909ccb43a7db52b6f2d791a6ea4
Tom Hoppe <tom@perfectsensedigital.com>
2014-01-02 14:47:13 -0500

    Updating gitignore and main.js

--- src/main/webapp/assets/scripts/main.js
+++ src/main/webapp/assets/scripts/main.js
@@ -1,43 +1,27 @@
-/*global define, require, $ */
 (function () {
     'use strict';
 
     require.config({
         name: "main",
-        insertRequire: ['main'],
         baseUrl: '/assets/scripts',
-        /*  we use a map to a blank jquery module here since jquery is already
-            included on the page and we want to use that. otherwise, we'd just
-            include a path to jquery in the paths defined below.
-         */
-        map: {
-            '*': {
-                'jquery': 'jquery.external'
-            }
-        },
+        // In the paths, you can specify which version of each module you'd like to use to create a shortcut. 
+        // Alternatively, you could specify the whole module name any time you needed it
         paths: {
-            'backbone': 'vendor/backbone/backbone-1.1.0',
-            'backbone-pageable': 'vendor/backbone/backbone-pageable',
-            'underscore': 'vendor/backbone/underscore-1.5.2', // required for backbone
-            'handlebars': 'vendor/handlebars-1.1.2'
+            'jquery': 'modules/jquery/jquery-1.10.2',
         },
-        shim: {
-            'vendor/jquery/jquery.ellipsis': ['jquery'],
-            'vendor/jquery/jquery.mobile.events': ['jquery'],
-            'vendor/jquery/jquery.mousewheel': ['jquery'],
-            'vendor/bootstrap/transition': ['jquery'],
-            'vendor/bootstrap/carousel': ['jquery','vendor/bootstrap/transition'],
-            'vendor/bootstrap/collapse': ['jquery', 'vendor/bootstrap/transition'],
-            'vendor/bootstrap/dropdown': ['jquery'],
-            'vendor/bootstrap/modal': ['jquery']
-        },
-
         wrap: true
     });
 
-    define(['views/app-view'], function (AppView) {
-        
-        
+    define(['modules/cookie/psd.cookie'], function (module) {
+
+        console.log(module);
 
     });
+
+    define(['jquery'], function($) {
+
+        console.log($);
+
+    });
+
 }());
